a05fc478606c6ad57b8f3b2df8633e5e143dca91
TA2LSM <semihsenol@gmail.com>
2023-10-01 19:48:56 +0300

    commit

--- WhatsAppCL/Model/FUser.swift
+++ WhatsAppCL/Model/FUser.swift
@@ -106,5 +106,93 @@ class FUser {
         })
     }
     
-    //49:57'den devam...
+    // MARK: Register function
+    class func registerUserWithEmail(email: String, password: String, fullName:String, avatar: String, phoneNumber: String, countryCode: String, completion: @escaping (_ error: Error?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { (firUser, error) in
+            if error != nil {
+                completion(error)
+                return
+            }
+            
+            let fUser = FUser(objectId: firUser!.user.uid, email: firUser!.user.email!, fullName: fullName, avatar: avatar, phoneNumber: phoneNumber, countryCode: countryCode, isOnline: false, contacts: [""] )
+            
+            saveUserLocally(fUser: fUser)
+            saveUserToFirestore(fUser: fUser)
+            completion(error)
+        }
+    }
+    
+    // MARK: Logout function
+    class func logOutCurrentUser(completion: @escaping (_ success: Bool) -> Void) {
+        userDefaults.removeObject(forKey: kCURRENTUSER)
+        userDefaults.synchronize()
+        
+        do {
+            try Auth.auth().signOut()
+            completion(true)
+        } catch let error as NSError {
+            completion(false)
+            print(error.localizedDescription)
+        }
+    }
+    
+    // MARK: Delete user
+    class func deleteUser(completion: @escaping (_ error: Error?) -> Void) {
+        let user = Auth.auth().currentUser
+        
+        user?.delete(completion: {(error) in
+            completion(error)
+        })
+    }
 }
+
+// MARK: Save user functions
+func saveUserToFirestore(fUser: FUser) {
+    collectionReference(.User)
+        .document(fUser.objectId)
+        .setData(userDictionatyFrom(user: fUser) as! [String: Any]) { (error) in
+            print("error is \(error?.localizeDescription)")}
+}
+
+func saveUserLocally(fUser: FUser) {
+    UserDefaults.standard.set(userDictionaryFrom(user: fUser), forKey: kCURRENTUSER)
+    UserDefaults.standard.synchronize()
+}
+
+// MARK: Fetch user functions
+// New firestore
+func fetchCurrentUserFromFirestore(userId: String) {
+    collectionReference(.User)
+        .document(userId)
+        .getDocument { (snapshot, error) in
+            guard let snapshot = snapshot else { return }
+            
+            if snapshot.exists {
+                UserDefaults.standard.setValue(snapshot.data() as! NSDictionary, forKeyPath: kCURRENTUSER)
+                UserDefaults.standard.synchronize()
+            }
+        }
+}
+
+func fetchCurrentUserFromFirestore(userId: String, completion: @escaping (_ user: FUser?) -> Void) {
+    collectionReference(.User)
+        .document(userId)
+        .getDocument { (snapshot, error) in
+            guard let snapshot = snapshot else { return }
+            
+            if snapshot.exists {
+                let user = FUser(_dictionary: snapshot.data()! as NSDictionary)
+                completion(user)
+            } else {
+                completion(nil)
+            }
+        }
+    
+}
+
+// MARK: Helper functions
+
+
+
+
+//50:52'den devam...
